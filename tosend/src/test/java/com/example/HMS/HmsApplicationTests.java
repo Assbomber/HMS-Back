4e9310276e60abce89835573292881ca8b41c11d
Aman kumar <36842116+Assbomber@users.noreply.github.com>
2022-02-18 12:34:44 +0530

    Update HmsApplicationTests.java

--- tosend/src/test/java/com/example/HMS/HmsApplicationTests.java
+++ tosend/src/test/java/com/example/HMS/HmsApplicationTests.java
@@ -2,12 +2,24 @@ package com.example.HMS;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.HMS.entity.StaffMapper;
+import com.example.HMS.exception.DepartmentNotFoundException;
+	
 @SpringBootTest
 class HmsApplicationTests {
 
+	@Autowired
+	private StaffServiceImpl staffservice;
+	
 	@Test
-	void contextLoads() {
+	void contextLoads() throws DepartmentNotFoundException {
+		StaffMapper staffMapper=StaffMapper.builder()
+			.role(Role.ADMIN.name())
+			.password("Admin")
+			.name("Admin")
+			.build();
+		System.out.println(staffservice.saveStaff(staffMapper));
 	}
 
 }
