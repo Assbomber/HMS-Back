f94d1214b65044ffc0fb2d4e9df0eb240f481093
Aman kumar <36842116+Assbomber@users.noreply.github.com>
2022-02-18 12:51:01 +0530

    Update SecurityConfig.java

--- tosend/src/main/java/com/example/HMS/security/SecurityConfig.java
+++ tosend/src/main/java/com/example/HMS/security/SecurityConfig.java
@@ -62,6 +62,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers(HttpMethod.POST,"/login").permitAll();
 //        http.authorizeRequests().antMatchers(HttpMethod.GET,"/login").permitAll();
+         http.authorizeRequests()
+        .antMatchers("/h2-console/**").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET,"/**").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET,"/hmsapi/admin/**").hasAuthority(Role.ADMIN.name());
 
